0923f0d226d8f8d7a3f503ef9b0fc3d28c398fd8
IoT Inventor <iotinventor@gmail.com>
2022-02-12 18:01:32 +0700

    change com to org in config file

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$base_url = "https://app.iotinventor.com/";
+$base_url = "https://app.iotinventor.org/";
 if (ENVIRONMENT !== 'production') {
 	$base_url = "http://localhost/iotinventor/";
 }
